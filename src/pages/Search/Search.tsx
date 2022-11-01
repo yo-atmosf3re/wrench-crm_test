e4177a3f35e5ed7e9f414574e75e0d6eabfe6654
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-11-02 00:54:14 +0300

    feat: реализована логика по отрпавке запроса на сервер с помощью клика;

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,42 +1,40 @@
-import React, { ChangeEvent, useCallback, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { SVG_PATH, universalSvg } from '../../utils/universalSvg';
 
-const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
-const token = "3815df32b28623d15cde2b203d0791cbfa06b5a0";
-const query = 'Москва'
-const options = {
-  method: "POST",
-  mode: "cors",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Authorization": "Token " + token,
-    withCredentials: true,
-  },
-  body: JSON.stringify({ query: query })
-};
+const Url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+const Token = "3815df32b28623d15cde2b203d0791cbfa06b5a0";
 
 export interface SuggestionsType {
   value: string
   unrestricted_value: string
-  data: any
+  data: Record<string, string | null>
 }
 
 const Search = () => {
-  // const [suggestions, setSuggestions] = useState<any>('Loading');
-  // useEffect(() => {
-  //   (async () => {
-  //     fetch(url, options as any)
-  //       .then(response => response.json())
-  //       .then(data => setSuggestions(data))
-  //       .catch(error => console.log(error))
-  //   })()
-  // }, [])
-
+  let [suggestions, setSuggestions] = useState([]);
   const [inputText, setInputText] = useState('')
   const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
+  const handleClick = async () => {
+    try {
+      const response = await fetch(Url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": "Token " + Token,
+        },
+        body: JSON.stringify({ query: inputText })
+      })
+      const result = await response.json()
+      setSuggestions(result)
+      console.log(suggestions)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className='main__search'>
@@ -52,15 +50,19 @@ const Search = () => {
           onChange={inputHandler}
           placeholder='Введите интересующий вас адрес'
           className="input-block__field" />
-        <button className='input-block__button'>
+        <button
+          onClick={handleClick}
+          className='input-block__button'>
           {universalSvg(SVG_PATH.SEARCH_BUTTON_ICON, '#ffffff')}
           <p>Поиск</p>
         </button>
       </div>
       <div className="search__address-block">
         <div className="address-block__title">Адреса</div>
-        <div className="address-block__search-result">
-        </div>
+        {
+          <div className="address-block__search-result">
+          </div>
+        }
       </div>
     </div>
   )
